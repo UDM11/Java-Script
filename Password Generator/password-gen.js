76f8701a70fd71fb814cfc4c72c466886f4370a4
Umesh Darlami <darlamiumesh123@gmail.com>
2025-06-30 19:45:11 +0545

    Password Generator

---
+++ Password Generator/password-gen.js
@@ -0,0 +1,13 @@
+function generatePassword() {
+  const length = document.getElementById("length").value;
+
+  const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=<>?";
+  let password = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * chars.length);
+    password += chars[randomIndex];
+  }
+
+  document.getElementById("result").textContent = password;
+}
